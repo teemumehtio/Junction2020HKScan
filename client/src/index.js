422d939215b4ac09a9c4041c39a8ccb4c35f57ad
Teemu Mehtiö <teemu.mehtio@businesslab.fi>
2020-11-07 14:04:53 +0200

    Initial client project.

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => {
+  return "hello world";
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
